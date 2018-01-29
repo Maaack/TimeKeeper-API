f71615bada452df749ecf32e7a81539c538e6bca
Maaack <marek.belski@gmail.com>
2018-01-29 15:42:33 -0800

    Models Revamp: timeline->timekeeper app

--- timekeeper/urls.py
+++ timekeeper/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from timelines import views
+from timekeeper import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
--- timelines/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TimekeeperBaseConfig(AppConfig):
-    name = 'timelines'
